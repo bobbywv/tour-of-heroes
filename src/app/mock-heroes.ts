df3597f20b3381b17e3728f40c19bccff1b828b9
Bobby Vanhoose <bobbywv@users.noreply.github.com>
2024-02-23 20:52:26 -0500

    completing section 2 of the tour-of-heroes tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Jesse Faden' },
+    { id: 13, name: 'Ephraim Winslow' },
+    { id: 14, name: 'Alan Wake' },
+    { id: 15, name: 'Leon Kennedy' },
+    { id: 16, name: 'Ward Phillips' },
+    { id: 17, name: 'Edward Pierce' },
+    { id: 18, name: 'Ethan Winters' },
+    { id: 19, name: 'Ranni' },
+    { id: 20, name: 'Miquella' },
+]
